4b850be2fc069cf854be29c882fffdedc45c8866
courtandrey <sudarkinandrew@gmail.com>
2022-09-11 17:20:18 +0300

    refrac: renamed packages

---
+++ src/main/java/SUDRFScrapper/Main.java
@@ -0,0 +1,11 @@
+package enter;
+
+import SUDRFScrapper.*;
+import SUDRFScrapper.view.SimpleSwingView;
+
+
+public class Main {
+    public static void main(String[] args)  {
+        new SUDRFScraper(new SimpleSwingView());
+    }
+}
